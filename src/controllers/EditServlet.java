3b3e031db00f8be6448b7b2021218ab949cfcc28
yuuki262 <xyz0262zzz@gmail.com>
2020-01-05 14:13:01 +0900

    Modify null control

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -44,8 +44,9 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
 
         // メッセージIDをセッションスコープに登録
+        if(m != null) {
         request.getSession().setAttribute("task_id", m.getId());
-
+        }
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/task/edit.jsp");
         rd.forward(request, response);
     }
